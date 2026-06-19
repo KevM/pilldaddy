4cbe7a90704abdf632f45424602def768c10d0f0
Kevin Miller <kevin@fm.rodeo>
2026-06-19 11:29:32 -0500

    Wire CloudKit-backed ModelContainer into PillDaddyApp

--- PillDaddy/PillDaddyApp.swift
+++ PillDaddy/PillDaddyApp.swift
@@ -1,10 +1,27 @@
 import SwiftUI
+import SwiftData
 
 @main
 struct PillDaddyApp: App {
+    let container: ModelContainer
+
+    init() {
+        do {
+            let config = ModelConfiguration(
+                schema: PillDaddySchema.schema,
+                isStoredInMemoryOnly: false,
+                cloudKitDatabase: .automatic
+            )
+            container = try ModelContainer(for: PillDaddySchema.schema, configurations: config)
+        } catch {
+            fatalError("Failed to initialize ModelContainer: \(error)")
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             MainTabView()
         }
+        .modelContainer(container)
     }
 }
